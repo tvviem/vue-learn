e1875c841a6a2f80fbe216d00513138909b9c8e8
viem trieu <trieuvinhviem@gmail.com>
2018-06-16 08:13:16 +0700

    Intro VueJS - Lesson 4  v-for

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 Intro vue basic from vuemastery.com
 1. The vue instance
 2. Attribute Binding
-a.  <img v-bind:src="image" >
-b. Shorthand 
-    :src = "image"
-    :alt = "description"
-    :href = "urlpath"
-    :title = "tooltip"
-    :class = "isActived"
-    :style = "isStyled"
-    :disabled = "isDisabled"
-c. Challenge
-    :href with binding hyperlink go to amazon search
+...
+4. List Rendering with for <br />
+a/ v-for details, variants <br />
+b/ solution for Challenge
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var product = 'Sock';
 var app = new Vue({
     el: '#app',
     data: {
@@ -6,7 +5,8 @@ var app = new Vue({
         image: './assets/images/vmSocks-green.jpeg',
         link: 'http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
-        inventory: 8,
-        onSale: false
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [{variantId: 2234, variantColor: 'green'}, {variantId: 2235, variantColor: 'blue'}],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL']
     }
 })
